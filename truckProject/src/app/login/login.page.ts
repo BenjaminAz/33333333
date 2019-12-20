5523d690a1b8ac4296c724e7c555bc840246cc64
BenjaminAz <52184788+BenjaminAz@users.noreply.github.com>
2019-12-20 12:22:11 -0300

    Cambios necesarios

--- truckProject/src/app/login/login.page.ts
+++ truckProject/src/app/login/login.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../models/employee';
-import { Client } from '../models/client';
+
 import { Package } from '../models/package';
 
 @Component({
@@ -11,8 +10,7 @@ import { Package } from '../models/package';
 export class LoginPage implements OnInit {
 
   package: Package[]
-  client: Client[]
-  employee: Employee[]
+
   constructor() { }
 
   ngOnInit() {
