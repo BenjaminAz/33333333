7ab155458a2c2fe8b37b647fa512736b6b87c455
Yamil Ali <yamil.ali@neoris.cxnetworks.net>
2019-12-23 09:12:21 -0300

    cambios login y registration

--- truckProject/src/app/services/auth.service.ts
+++ truckProject/src/app/services/auth.service.ts
@@ -1,18 +1,73 @@
 import { Injectable } from '@angular/core';
 import { Package } from '../models/package';
 import { Office } from '../models/office';
-import {  User } from '../models/user';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { NgxQRCodeComponent } from 'ngx-qrcode2';
+
+import { User } from '../models/user';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Toasts } from '../assets/toasts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  office: Office[];
-  user: User[];
+  public isLogged: any = false;
+	
+
+  user: firebase.User;
+  userInfo: User[];
   package: Package[];
-  constructor(private Barcode : BarcodeScanner, private NgxDatatable: NgxDatatableModule, private NgxQRScanner: NgxQRCodeComponent) { }
+  office: Office[];
+
+  constructor(private afAuth: AngularFireAuth,private Barcode : BarcodeScanner, private toastSvc: Toasts, private NgxDatatable: NgxDatatableModule, private NgxQRScanner: NgxQRCodeComponent) { 
+    //afAuth.authState.subscribe((user) => (this.isLogged = user));
+  }
+
+
+	//Login
+	async onLogin(user) {
+		try {
+      return await this.afAuth.auth.signInWithEmailAndPassword(
+        user.email,
+        user.password
+        )
+      .then(userCredential => {
+        
+      })
+		} catch (error) {
+			var errorCode = error.code;
+			if (errorCode == 'auth/invalid-email') {
+				this.toastSvc.emailNotValidToast()
+			} else if (errorCode == 'auth/user-not-found') {
+				this.toastSvc.userNotFound();
+			} 
+		}
+	}
+
+//registro
+async onRegister(user) {
+  try {
+    return await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+  } catch (error) {
+    if (error.code == 'auth/user-not-found') {
+      this.toastSvc.userNotFound();
+    } else if (error.code == 'auth/weak-password') {
+      this.toastSvc.charPasswordToast();
+    } else if (error.code == 'auth/email-already-in-use') {
+      this.toastSvc.emailUsedToast();
+    }
+    console.log('Error on register user', error);
+  }
+}
+
+
+
+
+
+
+
+
 }
